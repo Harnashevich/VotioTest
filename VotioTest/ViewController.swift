cc93f1bf2b7317e93feeb196e7d3bc56fe1f9f14
Andrei <garnash1313@gmail.com>
2024-03-26 16:20:38 +0300

    Initial Commit

---
+++ VotioTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VotioTest
+//
+//  Created by Andrei Harnashevich on 26.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
